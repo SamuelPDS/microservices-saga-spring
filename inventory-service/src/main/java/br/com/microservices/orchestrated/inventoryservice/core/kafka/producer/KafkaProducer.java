89aadd8915d3f70372e97c3a6ebc1bdc516e1c1a
Samuel <duartes607@gmail.com>
2025-01-31 20:16:39 -0300

    cria producers

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/kafka/producer/KafkaProducer.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.inventoryservice.core.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor // cria o construtor com os campos necessarios
+@Component
+public class KafkaProducer {
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            log.info("sending event to Topic {} with data {}", orchestratorTopic, payload);
+            kafkaTemplate.send(orchestratorTopic, payload);
+        } catch (Exception e) {
+            log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload);
+        }
+    }
+}
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/kafka/producer/SagaProducer.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.orderservice.core.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor // cria o construtor com os campos necessarios
+@Component
+public class SagaProducer {
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            log.info("sending event to Topic {} with data {}", startSagaTopic, payload);
+            kafkaTemplate.send(startSagaTopic, payload);
+        } catch (Exception e) {
+            log.error("Error trying to send data to topic {} with data {}", startSagaTopic, payload);
+        }
+    }
+}
